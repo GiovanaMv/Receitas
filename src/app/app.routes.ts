c166b72c90248f53f8d91c340ef4c980fec45d0a
GiovanaMv <saddnez@gmail.com>
2025-08-30 22:22:21 -0300

    feat: add footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,29 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { MenuComponent } from './pages/menu/menu';
+import { PaoGraos } from './pages/comidas/pao-graos/pao-graos';
+import { SaladaLegumes } from './pages/comidas/salada-legumes/salada-legumes';
+import { SopaAbobora } from './pages/comidas/sopa-abobora/sopa-abobora';
+import { SucoCenoura } from './pages/refrescos/suco-cenoura/suco-cenoura';
+import { SucoManga } from './pages/refrescos/suco-manga/suco-manga';
+import { SucoMelancia } from './pages/refrescos/suco-melancia/suco-melancia';
+import { PanquecaChocolate } from './pages/doces/panqueca-chocolate/panqueca-chocolate';
+import { GeleiaPessego } from './pages/doces/geleia-pessego/geleia-pessego';
+import { BiscoitoCanela } from './pages/doces/biscoito-canela/biscoito-canela';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent},
-  { path: 'menu', component: MenuComponent}
+  { path: 'menu', component: MenuComponent},
+
+  { path: 'receita/pao-com-graos', component: PaoGraos },
+  { path: 'receita/salada-legumes', component: SaladaLegumes },
+  { path: 'receita/sopa-abobora', component: SopaAbobora },
+
+  { path: 'receita/suco-manga', component: SucoManga },
+  { path: 'receita/suco-melancia', component: SucoMelancia },
+  { path: 'receita/suco-cenoura', component: SucoCenoura },
+
+  { path: 'receita/biscoito-canela', component: BiscoitoCanela },
+  { path: 'receita/geleia-pessego', component: GeleiaPessego },
+  { path: 'receita/panqueca-chocolate', component: PanquecaChocolate },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer';
+import { HeaderComponent } from './components/header/header';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FooterComponent,
+    HeaderComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('minhas-receitas');
+  showFooter = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.showFooter = event.url !== '/login';
+      });
+  }
 }
